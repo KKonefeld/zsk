6b5f2629d5777bf0434115f73f3e0a477abc3448
KKonefeld <54834569+KKonefeld@users.noreply.github.com>
2019-10-08 09:33:13 +0200

    Add files via upload

---
+++ 8,10/formularz5.1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <?php
+          if (!empty($_GET['country'])) {
+            echo "ok";
+          }else {
+            ?>
+
+      <form method="get">
+        <input type="text" name="country" value="" placeholder="Podaj Kraj">
+        <input type="submit" name="" value="Zatwierdz">
+      </form>
+  
+  <?php
+    }
+
+
+   ?>
+</body>
+
+</html>
