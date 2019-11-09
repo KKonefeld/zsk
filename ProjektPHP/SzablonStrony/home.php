1a4dd67c6a51b25eb3dc5ca39afbd0463fa235ef
KKonefeld <54834569+KKonefeld@users.noreply.github.com>
2019-11-09 22:44:38 +0100

    Male kodowanko dla zabawy// poprawiona przejrzystosc, dodane hamburger menu

--- ProjektPHP/SzablonStrony/home.php
+++ ProjektPHP/SzablonStrony/home.php
@@ -11,47 +11,119 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="style/home.css">
+      <link rel="stylesheet" href="style/mainlayout.css">
     <title>Quizletor</title>
   </head>
   <body>
-
-
-
-
-
-
-
     <div class="container ">
-  <div class="row rowek sticky-top">
-    <div class="col-6 col-xl-3">
-      <button type="button" class="buttn btn btn-primary" >
-        <img src="grafika/logo.png" alt="Logo">
-      </button>
-    </div>
+      <!-- menu glowne -->
+      <div class="row rowek sticky-top">
+        <div class="col-6 col-xl-3">
+          <button type="button" class="przyciskimenu btn btn-primary" >
+            <img src="grafika/logo.png" alt="Logo">
+          </button>
+        </div>
 
-    <div class="col-4 col-xl-3">
-    <a href="home.php">  <button type="button" class="buttn btn btn-primary" >
-        <img src="grafika/home.png" alt="Home">
-      </button> </a>
-    </div>
-    <div class="col-4 col-xl-3">
-        <a href="logowanie.php"> <button type="button" class="buttn btn btn-primary"><img src="grafika/login.png" alt="Logowanie"></button> </a>
-    </div>
-    <div class="col-4 col-xl-3">
-      <a href="rejestracja.php">  <button type="button" class="buttn btn btn-primary"><img src="grafika/register.png" alt="Rejestracja" ></button> </a>
-    </div>
-  </div>
+        <div class="col-4 col-xl-3">
+        <a href="home.php">  <button type="button" class="przyciskimenu btn btn-primary" >
+            <img src="grafika/home.png" alt="Home">
+          </button> </a>
+        </div>
+        <div class="col-4 col-xl-3">
+            <a href="logowanie.php"> <button type="button" class="przyciskimenu btn btn-primary"><img src="grafika/login.png" alt="Logowanie"></button> </a>
+        </div>
+        <div class="col-4 col-xl-3">
+          <a href="rejestracja.php">  <button type="button" class="przyciskimenu btn btn-primary"><img src="grafika/register.png" alt="Rejestracja" ></button> </a>
+        </div>
+
+
+      </div>
   <div class="row">
       <div class="col-12 col-xl-4 kurs rounded">
-      
 
+        <!-- inicjacja hamburger menu -->
+        <nav class="navbar wyborkursu">
 
+          <!-- Tytul listy rozwijanej -->
+          <a class="navbar-brand" href="#">E12</a>
 
+          <!-- Strzalka od rozwijania -->
+          <img src="grafika/strzalka.png" alt="" data-toggle="collapse" data-target="#navbarSupportedContent1">
 
+          <!-- Ukryte, rozwijane warstwy menu -->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent1">
 
+            <!-- Podlinki -->
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Linux</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Windows</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">UTK</a>
+              </li>
+            </ul>
+            <!-- Tu sie koncza podlinki -->
+          </div>
+        </nav>
+        <!-- inicjacja hamburger menu -->
+        <nav class="navbar wyborkursu">
 
+          <!-- Tytul listy rozwijanej -->
+          <a class="navbar-brand" href="#">E13</a>
 
+          <!-- Strzalka od rozwijania -->
+          <img src="grafika/strzalka.png" alt="" data-toggle="collapse" data-target="#navbarSupportedContent2">
+
+          <!-- Ukryte, rozwijane warstwy menu -->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+
+            <!-- Podlinki -->
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sieci</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Urzadzenia</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Protokoly</a>
+              </li>
+            </ul>
+            <!-- Tu sie koncza podlinki -->
+          </div>
+        </nav>
+        <!-- inicjacja hamburger menu -->
+        <nav class="navbar wyborkursu">
+
+          <!-- Tytul listy rozwijanej -->
+          <a class="navbar-brand" href="#">E14</a>
+
+          <!-- Strzalka od rozwijania -->
+          <img src="grafika/strzalka.png" alt="" data-toggle="collapse" data-target="#navbarSupportedContent3">
+
+          <!-- Ukryte, rozwijane warstwy menu -->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent3">
+
+            <!-- Podlinki -->
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">PHP</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">WebDesign</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">BazyDanych</a>
+              </li>
+            </ul>
+            <!-- Tu sie koncza podlinki -->
+          </div>
+        </nav>
 
 
       </div>
@@ -66,12 +138,10 @@
 
   </div>
 
-
-
-
-
 </div>
 <div id="ikony">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+
+
 
   </body>
 </html>
--- ProjektPHP/SzablonStrony/logowanie.php
+++ ProjektPHP/SzablonStrony/logowanie.php
@@ -12,34 +12,31 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style/logowanie.css">
+    <link rel="stylesheet" href="style/mainlayout.css">
     <title>Quizletor</title>
   </head>
   <body>
 
 
-
-
-
-
-
     <div class="container ">
+
   <div class="row rowek sticky-top">
     <div class="col-6 col-xl-3">
-      <button type="button" class="buttn btn btn-primary" >
+      <button type="button" class="przyciskimenu btn btn-primary" >
         <img src="grafika/logo.png" alt="Logo">
       </button>
     </div>
 
     <div class="col-4 col-xl-3">
-    <a href="home.php">  <button type="button" class="buttn btn btn-primary" >
+    <a href="home.php">  <button type="button" class="przyciskimenu btn btn-primary" >
         <img src="grafika/home.png" alt="Home">
       </button> </a>
     </div>
     <div class="col-4 col-xl-3">
-        <a href="logowanie.php"> <button type="button" class="buttn btn btn-primary"><img src="grafika/login.png" alt="Logowanie"></button> </a>
+        <a href="logowanie.php"> <button type="button" class="przyciskimenu btn btn-primary"><img src="grafika/login.png" alt="Logowanie"></button> </a>
     </div>
     <div class="col-4 col-xl-3">
-      <a href="rejestracja.php">  <button type="button" class="buttn btn btn-primary"><img src="grafika/register.png" alt="Rejestracja" ></button> </a>
+      <a href="rejestracja.php">  <button type="button" class="przyciskimenu btn btn-primary"><img src="grafika/register.png" alt="Rejestracja" ></button> </a>
     </div>
 
 
